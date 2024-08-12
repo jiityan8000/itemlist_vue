0e22748dcc31e71e9cd65c5551dbb819a8f2d69d
Hiroki Nagai <jiityan8000@gmail.com>
2024-08-12 12:23:45 +0900

    ベースのレイアウトをコンポーネント分け

---
+++ src/components/layout/BaseLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="BaseLayout">
+    <slot name="header"></slot>
+    <slot name="main"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+<style lang="scss">
+.BaseLayout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100dvh;
+}
+</style>
